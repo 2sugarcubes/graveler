214e18a093bb27df26474de345a8aae3a4887ff5
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 23:05:29 -0700

    Actually using all cores now

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use rayon::prelude::*;
 
 fn main() {
     const ONE_BILLION_ROWS: u64 = 1_000_000_000;
-    let step_size = ONE_BILLION_ROWS / 8;
+    let step_size = ONE_BILLION_ROWS / 12 + 1;
 
-    let max_ones = (0_usize..8)
+    let max_ones = (0_usize..12)
         .into_par_iter()
         .map(|_| check_n_games(step_size))
         .max()
         .unwrap_or(0_u32);
 
     println!("Highest Ones Roll: {}", max_ones);
-    println!("Number of Roll Sessions: {}", step_size * 8);
+    println!("Number of Roll Sessions: {}", step_size * 12);
 }
 
 fn check_n_games(n: u64) -> u32 {
