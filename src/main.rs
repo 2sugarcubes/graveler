84f68f5d9a7daafa64ab869b532750713c6b580e
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 21:29:28 -0700

    BUGFIX, wasn't resetting ones count

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ fn main() {
     const ONE_MILLION_DOLLARS: u64 = 1_000_000;
     for _ in 0..ONE_MILLION_DOLLARS {
         for _ in 0..231 {
-            let rand_number: u8 = rng.next();
-            if rand_number % 4 == 1 {
+            let rand_number: u8 = rng.gen();
+            if rand_number % 4 == 0 {
                 ones += 1;
             }
         }
         if ones > max_ones {
             max_ones = ones;
         }
+        ones = 0;
     }
 
-    println!("Highest Ones Roll: {}", ones);
+    println!("Highest Ones Roll: {}", max_ones);
     println!("Number of Roll Sessions: {}", ONE_MILLION_DOLLARS);
 }
