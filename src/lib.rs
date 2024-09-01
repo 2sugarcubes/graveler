c808559cbb84f152727ab290f768bb91e52fced0
BuildTools <unconfigured@null.spigotmc.org>
2024-08-31 21:09:35 -0700

    First version of cheeky generation

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,52 @@ pub fn check_n_games(n: u64) -> u32 {
     return max_ones;
 }
 
+pub fn pregenerate_12911_half_games() -> [[u64; 4]; 12_911] {
+    let mut results: [[u64; 4]; 12911] = [[0; 4]; 12911];
+    let mut rng: u64 = rand::thread_rng().gen();
+    let mut inner: [u64; 4] = [0, 0, 0, 0];
+
+    for i in 0..1290 {
+        for j in 0..4 {
+            inner[j] = rng;
+
+            rng ^= rng << 7;
+            rng ^= rng >> 9;
+        }
+        results[i] = inner;
+    }
+
+    results
+}
+
+pub fn play_game_sets(half_games: [[u64; 4]; 12_911]) -> (u32, u64) {
+    let mut max_ones = 0;
+    let mut count = 0;
+
+    for i in 0..12910 {
+        let game_a = half_games[i];
+        for j in (i + 1)..12911 {
+            let game_b = half_games[j];
+            let mut ones = (game_a[0] & game_b[0]).count_ones();
+            ones += (game_a[1] & game_b[1]).count_ones();
+            ones += (game_a[2] & game_b[2]).count_ones();
+            ones += (game_a[3] & game_b[3] & 0x7F_FF_FF_FF_FF).count_ones();
+
+            count += 1;
+            if ones > max_ones {
+                max_ones = ones;
+            }
+        }
+    }
+
+    (max_ones, count)
+}
+
+pub fn check_from_half_games() -> (u32, u64) {
+    play_game_sets(pregenerate_12911_half_games())
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct QuickerRng {
     pub state_1: u64,
     pub state_2: u64,
@@ -62,5 +108,17 @@ impl QuickerRng {
     pub fn get_chances(&mut self) -> u64 {
         self.next_state();
         self.state_1 & self.state_2
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn check_game_count() {
+        let half_games = pregenerate_12911_half_games();
+        let results = play_game_sets(half_games);
+        println!("Played {} games with at most {} ones", results.1, results.0);
+        assert!(results.1 > 1_000_000_000 / 12);
     }
 }
